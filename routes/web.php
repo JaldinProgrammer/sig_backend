04d7793d8c6ec20c6e48702f330384e4a99818af
jsoliz064 <jsoliz064@gmail.com>
2022-07-13 18:28:40 -0400

    xd

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\CarModelController;
 use App\Http\Controllers\LicenseCategoryController;
 use App\Http\Controllers\VehicleController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\sessionController;
+use App\Http\Controllers\SessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,6 +32,6 @@ Route::get('modelos',[CarModelController::class,'index'])->name('modelos.index')
 Route::get('licencias',[LicenseCategoryController::class,'index'])->name('licencias.index');
 Route::get('vehiculos',[VehicleController::class,'index'])->name('vehiculos.index');
 Route::get('choferes',[UserController::class,'index'])->name('users.index');
-Route::get('sessions',[sessionController::class,'session']);
+Route::get('sessions',[SessionController::class,'session']);
 
 
